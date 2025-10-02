49dda409c4d6f6efbca398c011309f3d14095f98
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2025-10-02 09:45:51 -0300

    commit5

--- app/src/main/java/com/EA2vicenteOyarzun/MainActivity.kt
+++ app/src/main/java/com/EA2vicenteOyarzun/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.EA2vicenteOyarzun
 
+import android.R
+import androidx.compose.ui.graphics.Color
 import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
@@ -27,27 +32,43 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
+import android.graphics.*
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
 import com.EA2vicenteOyarzun.ui.theme.EA2vicenteOyarzunTheme
+import org.intellij.lang.annotations.JdkConstants
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TextBox()
+            LayoutMain()
         }
         }
     }
-@Preview
 @Composable
 fun TextBox(){
     var txt by remember { mutableStateOf("") }
-    Column {
-        Spacer(modifier = Modifier.padding(12.dp))
-        TextField(
-            value = txt,
-            onValueChange = { it ->
-                txt = it
-            },
-            label = { Text("caca") }
-        )
+    TextField(
+        value = txt,
+        onValueChange = {
+            it -> txt = it },
+        label = { Text("caca") })
+}
+@Composable
+fun LayoutMain(){
+    Column(
+        Modifier.fillMaxHeight().background(Color.Red),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.background(Color.Yellow).fillMaxWidth()
+        ) {
+            TextBox()
+        }
     }
 }
